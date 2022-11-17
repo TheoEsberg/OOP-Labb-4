e94a6687fd6c223c6bcf6a3b700aaa9709508b82
Theo Esberg <esbergtheo@gmail.com>
2022-11-17 15:03:26 +0100

    Create an Employee class

---
+++ OOP-Labb-4/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Labb_4
+{
+    internal class Employee
+    {
+        private int ID { get; set; }
+        private string Name { get; set; }
+        private string Gender { get; set; }
+        private int Salary { get; set; }
+
+        public Employee(int id, string name, string gender, int salary)
+        {
+            this.ID = id;
+            this.Name = name;
+            this.Gender = gender;
+            this.Salary = salary;
+        }
+
+        public Employee()
+        {
+
+        }
+    }
+}
