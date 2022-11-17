70baa5a585069fb5a58c99c0c6acbdd4a2967668
Theo Esberg <esbergtheo@gmail.com>
2022-11-17 15:55:54 +0100

    Enhanced readability with regions

--- OOP-Labb-4/Program.cs
+++ OOP-Labb-4/Program.cs
@@ -7,24 +7,31 @@ namespace OOP_Labb_4
     {
         static void Main(string[] args)
         {
+            #region Create Employees
             // Create five different Employees
             Employee E1, E2, E3, E4, E5;
             CreateEmployees(out E1, out E2, out E3, out E4, out E5);
+            #endregion
 
+            #region Create Employees Stack
             // Stack
             Stack<Employee> EmpStack = new Stack<Employee>();
             AddEmployeesToStack(E1, E2, E3, E4, E5, EmpStack);
+            #endregion
 
+            #region Print Employees in EmpStack
             // Write out all of the Employees in EmpStack
             foreach (Employee e in EmpStack)
             {
                 Console.WriteLine("ID: {0}, Name: {1}, Gender: {2}, Salary: {3}kr", e.GetId(), e.GetName(), e.GetGender(), e.GetSalary());
                 Console.WriteLine("Employees left in stack: {0}", EmpStack.Count);
             }
+            #endregion
 
             // Make a new blank row for easier readability in console
             Console.WriteLine();
 
+            #region Pop Employees in EmpStack
             // Pop all of the Employees in EmpStack
             while (EmpStack.Count > 0)
             {
@@ -32,6 +39,7 @@ namespace OOP_Labb_4
                 Console.WriteLine("ID: {0}, Name: {1}, Gender: {2}, Salary: {3}kr", e.GetId(), e.GetName(), e.GetGender(), e.GetSalary());
                 Console.WriteLine("Employees left in stack: {0}", EmpStack.Count);
             }
+            #endregion
 
             // Add all of the Emplyees back to the stack
             AddEmployeesToStack(E1, E2, E3, E4, E5, EmpStack);
@@ -39,6 +47,7 @@ namespace OOP_Labb_4
             // Make a new blank row for easier readability in console
             Console.WriteLine();
 
+            #region Peek EmpStack
             // Get the info about the first employee in EmpStack two times with Peek
             for (int i = 0; i < 2; i++)
             {
@@ -46,14 +55,14 @@ namespace OOP_Labb_4
                 Console.WriteLine("ID: {0}, Name: {1}, Gender: {2}, Salary: {3}kr", e.GetId(), e.GetName(), e.GetGender(), e.GetSalary());
                 Console.WriteLine("Employees left in stack: {0}", EmpStack.Count);
             }
+            #endregion
 
+            #region Contains EmpStack
             // Check if employee three exists in the stack
             bool ExistsInStack = EmpStack.Contains(E3);
             if (!ExistsInStack) { Console.WriteLine("Employee E3 does not exist in EmpStack."); }
             else { Console.WriteLine("Employee E3 exist in EmpStack."); }
-
-
-
+            #endregion
         }
 
         private static void AddEmployeesToStack(Employee E1, Employee E2, Employee E3, Employee E4, Employee E5, Stack<Employee> EmpStack)
